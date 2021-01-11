4d6a8e019c51de9fbf53ab36283ae0e13fb029e3
remilepriol <remi.lp.17@gmail.com>
2021-01-11 21:22:46 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7674c51477d5c0ac855166fe2ac738a1.js"
+  "/precache-manifest.ea069df865c05c9cbc202707fc08edb4.js"
 );
 
 self.addEventListener('message', (event) => {
