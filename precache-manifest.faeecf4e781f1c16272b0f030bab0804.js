57e11e875bb0ec786740cee4f544a821650f6a7b
remilepriol <remi.lp.17@gmail.com>
2021-01-11 16:40:04 +0100

    Updates

---
+++ precache-manifest.faeecf4e781f1c16272b0f030bab0804.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88c71ea857995782028d47f3bfb3f12",
+    "url": "21-22.com/index.html"
+  },
+  {
+    "revision": "a874c3d2650a10266423",
+    "url": "21-22.com/static/css/main.1408478c.chunk.css"
+  },
+  {
+    "revision": "99c24c800c95fd8ff38e",
+    "url": "21-22.com/static/js/2.2b0bf620.chunk.js"
+  },
+  {
+    "revision": "a874c3d2650a10266423",
+    "url": "21-22.com/static/js/main.fa6912b0.chunk.js"
+  },
+  {
+    "revision": "9ae3d9019a1bcc51be72",
+    "url": "21-22.com/static/js/runtime~main.8f8b81a8.js"
+  }
+]);
