7207b9f2e77c1b1b132449890598e3131f38cd56
vrabaron <vrabaron@gmail.com>
2021-01-12 23:11:39 +0100

    Updates

---
+++ precache-manifest.46339299c63791f67db3522788b07617.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "980de1c1fac0980b3aaf597995cd093a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3272f5c804672ca8fa18",
+    "url": "/static/css/main.2bb63f11.chunk.css"
+  },
+  {
+    "revision": "d086d3bf1fc2bd74c878",
+    "url": "/static/js/2.b2716abb.chunk.js"
+  },
+  {
+    "revision": "3272f5c804672ca8fa18",
+    "url": "/static/js/main.c4964f51.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
